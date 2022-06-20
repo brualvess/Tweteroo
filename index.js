4e03d31764afb9297e85d5f27d0eb1393fc38c9e
brualvess <oliveirabrunna2210@gmail.com>
2022-06-20 08:01:42 -0300

    ajustes

--- index.js
+++ index.js
@@ -14,33 +14,19 @@ server.post("/sign-up", (request, response) => {
     usuario.push(body);
     response.send("Ok");
 })
-
-import cors from "cors";
-import express, {json} from "express";
-
-const app = express();
-app.use(json());
-app.use(cors());
-
-const tweets = [];
-const users = [];
-
-app.post("/sign-up", (request, response) => {
-    const body = request.body;
-    users.push(body);
-    response.send("Ok");
-})
-
-app.post("/tweets", (request, response) => {
+server.post("/tweets", (request, response) => {
     const body = request.body;
     const {username, tweet} = body;
-    const {avatar} = users.find(user => user.username === username);
+    const {avatar} = usuario.find(user => user.username === username);
     tweets.push({
         username,
         avatar,
         tweet
     })
     response.send("Ok");
+})
+server.get("/tweets", (request, response) => {
+  response.send([...tweets].reverse().splice(0, 10));
 })
 
 server.listen(5000)
